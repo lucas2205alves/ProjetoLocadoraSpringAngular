5adab42604b63d90a138a47678e911ff0a8e3db9
Lucas Ricardo Domingos Alves <lucas.alves2205@hotmail.com>
2024-09-23 22:27:12 -0300

    Continuação carroService
    implementado método no CarroService
    -save
    -update
    -findById

--- frontend/LocadoraFront/src/app/components/carros/carrosdetails/carrosdetails.component.ts
+++ frontend/LocadoraFront/src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -4,6 +4,7 @@ import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Carro } from '../../../models/carro';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2'
+import { CarroService } from '../../../services/carro.service';
 
 @Component({
   selector: 'app-carrosdetails',
@@ -17,6 +18,8 @@ export class CarrosdetailsComponent {
   carro: Carro = new Carro(0, "Carro", "Marca")
   router = inject(ActivatedRoute);
   router2 = inject(Router); // Redirecionamento
+
+  carroService = inject(CarroService);
   constructor(){
     let id = this.router.snapshot.params['id'];
     if(id > 0){
@@ -45,8 +48,17 @@ export class CarrosdetailsComponent {
   }
 
   findById(id: number){
-    // implementar busca no spring
-    let carroRetornado: Carro = new Carro(id, "Teste Edição", "Marca");
-    this.carro = carroRetornado;
+    this.carroService.findById(id).subscribe({
+      next: retorno => {
+          this.carro = retorno
+      },
+      error: erro => {
+        Swal.fire({
+          title: 'Ocorreu um erro',
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        });
+      }
+    })
   }
 } 
--- frontend/LocadoraFront/src/app/services/carro.service.ts
+++ frontend/LocadoraFront/src/app/services/carro.service.ts
@@ -21,4 +21,14 @@ export class CarroService {
   delete(id: number): Observable<string>{
     return this.http.delete<string>(this.API+"/delete/"+id, {responseType: 'text' as 'json'});
   }
+
+  save(carro: Carro): Observable<string>{
+    return this.http.post<string>(this.API+"/save", carro, {responseType: 'text' as 'json'});
+  }
+  update(carro: Carro, id: number): Observable<string>{
+    return this.http.put<string>(this.API+"/update/"+id, carro, {responseType: 'text' as 'json'});
+  }
+  findById(id: number): Observable<Carro>{
+    return this.http.get<Carro>(this.API+"/findById/"+id);
+  }
 }
