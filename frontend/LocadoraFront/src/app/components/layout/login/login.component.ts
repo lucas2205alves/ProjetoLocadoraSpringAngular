25ede55c956354937df624394b009ab18382e1c7
Lucas Ricardo Domingos Alves <lucas.alves2205@hotmail.com>
2024-10-04 18:12:29 -0300

    Autenticação com JWT
    Criado as classes tanto no server como front para utilizar autenticação pelo JWT

--- frontend/LocadoraFront/src/app/components/layout/login/login.component.ts
+++ frontend/LocadoraFront/src/app/components/layout/login/login.component.ts
@@ -2,26 +2,36 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { Login } from '../../../auth/login';
+import { LoginService } from '../../../auth/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  usuario!: string;
-  senha!: string;
+  login: Login = new Login();
 
+  loginService = inject(LoginService);
   router = inject(Router);
 
   logar() {
-    if (this.usuario == 'admin' && this.senha == 'admin') {
-      this.router.navigate(['admin/carros']);
-
-    } else
-      alert('Usuario ou senha incorretos')
+    this.loginService.logar(this.login).subscribe({
+      next: token => { // QUANDO DÁ CERTO
+        console.log(token);
+		if(token)
+			this.loginService.addToken(token); //MUITO IMPORTANTE
+        this.router.navigate(['/admin/carros']);
+      },
+      error: erro => { // QUANDO DÁ ERRO
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+      }
+    });
   }
+
+
 }
---
+++ server/Locadora/src/main/java/br/com/projetoLocadora/Locadora/auth/LoginService.java
@@ -0,0 +1,35 @@
+package br.com.projetoLocadora.Locadora.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import br.com.projetoLocadora.Locadora.config.JwtServiceGenerator;
+
+
+@Service
+public class LoginService {
+	
+	@Autowired
+	private LoginRepository repository;
+	@Autowired
+	private JwtServiceGenerator jwtService;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+
+	public String logar(Login login) {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(
+						login.getUsername(),
+						login.getPassword()
+						)
+				);
+		Usuario user = repository.findByUsername(login.getUsername()).get();
+		String jwtToken = jwtService.generateToken(user);
+		
+		return jwtToken;
+	}
+
+}
